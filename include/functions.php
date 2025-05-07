61633242d2b934ea88bd2bd3927c757b35253f4b
Philip Peterson <philip-peterson@users.noreply.github.com>
2025-05-07 02:55:00 -0700

    Fix

--- include/functions.php
+++ include/functions.php
@@ -3,7 +3,7 @@
 function print_page($title, $body_func, $html_attrs='', $body_attrs='') {
 
 ?><!doctype html>
-<html <?echo $html_attrs?>>
+<html <?php echo $html_attrs; ?>>
 <head>
    <title><?php if(!empty($title)) { echo "$title &ndash; ";} ?>Philip Peterson | Software Architect</title>
    <link href='https://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
@@ -11,7 +11,7 @@ function print_page($title, $body_func, $html_attrs='', $body_attrs='') {
    <script type='text/javascript' src='js/jquery.min.js'></script>
    <script type='text/javascript' src='js/growy.js'></script>
 </head>
-<body <? echo $body_attrs; ?>>
+<body <?php echo $body_attrs; ?>>
 
 <?php
    $body_func();
